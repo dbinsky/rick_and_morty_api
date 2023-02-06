a3a50ea4830eeea069dfb1985c4f451ff90227b8
Andrey Dubinsky <adreyka626@mail.ru>
2023-02-06 11:52:23 +0200

    Added repository and completed work on the data layer

--- lib/feature/domain/repositories/person_repository.dart
+++ lib/feature/domain/repositories/person_repository.dart
@@ -4,5 +4,5 @@ import 'package:rick_and_morty_api/feature/domain/entities/person_entity.dart';
 
 abstract class PersonRepository {
   Future<Either<Failure, List<PersonEntity>>> getAllPerson(int page);
-  Future<Either<Failure, List<PersonEntity>>> searchPerson(String quert);
+  Future<Either<Failure, List<PersonEntity>>> searchPerson(String query);
 }
