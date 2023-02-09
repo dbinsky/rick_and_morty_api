fcaa1728e644d8a5f5ad5758490584c19eb15162
Andrey Dubinsky <adreyka626@mail.ru>
2023-02-09 13:27:40 +0200

    Added locator_servise

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_api/feature/presentation/bloc/person_list_cubit/person_list_cubit.dart';
+import 'package:rick_and_morty_api/feature/presentation/bloc/search_bloc/search_bloc.dart';
+import 'package:rick_and_morty_api/feature/presentation/page/person_page.dart';
+import 'package:rick_and_morty_api/locator_servise.dart' as di;
+import 'locator_servise.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -9,21 +17,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<PersonListCubit>(
+            create: (context) => sl<PersonListCubit>()),
+        BlocProvider<PersonSearchBloc>(
+            create: (context) => sl<PersonSearchBloc>()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData.dark().copyWith(
+          colorScheme: const ColorScheme.dark(background: Colors.black),
+          scaffoldBackgroundColor: Colors.grey,
+        ),
+        home: const HomePage(),
+      ),
     );
   }
 }
