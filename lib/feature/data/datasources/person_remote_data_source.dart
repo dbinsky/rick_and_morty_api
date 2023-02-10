6f6a815383ede406fe1dae9d69526a6fe777305e
Andrey Dubinsky <adreyka626@mail.ru>
2023-02-10 14:24:58 +0200

    Fixed bugs

--- lib/feature/data/datasources/person_remote_data_source.dart
+++ lib/feature/data/datasources/person_remote_data_source.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'package:rick_and_morty_api/core/error/exception.dart';
 import 'package:rick_and_morty_api/feature/data/models/person_model.dart';
 import 'package:http/http.dart' as http;
@@ -13,20 +12,19 @@ class PersonRemoteDataSourceImplement implements PersonRemoteDataSource {
   final http.Client client;
 
   PersonRemoteDataSourceImplement({required this.client});
+
   @override
   Future<List<PersonModel>> getAllPersons(int page) => _getPersonFromUrl(
       'https://rickandmortyapi.com/api/character/?page=$page');
 
   @override
   Future<List<PersonModel>> searchPerson(String query) => _getPersonFromUrl(
-      'https://rickandmortyapi.com/api/character/?name=rick$query');
+      'https://rickandmortyapi.com/api/character/?name=$query');
 
   Future<List<PersonModel>> _getPersonFromUrl(String url) async {
-    log(url);
-    final response = await client.get(
-      Uri.parse(url),
-      headers: {'Content type': 'application/json'},
-    );
+    print(url);
+    final response = await client
+        .get(Uri.parse(url), headers: {'Content-Type': 'application/json'});
     if (response.statusCode == 200) {
       final persons = json.decode(response.body);
       return (persons['results'] as List)
